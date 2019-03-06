8ab036f022f843b72fe35670688863997dd88f66
Adeynack <Adeynack@users.noreply.github.com>
2019-03-06 11:20:41 +0100

    OAS get-post-patch with IDs and SLUGs

--- openapi/get-post-patch-id-slug/idea001/expected.ts
+++ openapi/get-post-patch-id-slug/idea001/expected.ts
@@ -1,6 +1,3 @@
-// Expected interfaces created from this OpenAPI Specification
-// https://gist.github.com/Adeynack/c42b928189eb4a227a4f42a10b6059b5
-
 type UUID = string // for the example
 
 export interface PersonPatch {
@@ -37,4 +34,30 @@ export interface Person {
     lastName: string
     birthdate: Date
     description?: string
+}
+
+class PersonAPI {
+
+    getPersonList(): Array<Person> {
+        // GET request to `/persons`
+        return [];
+    }
+
+    createPerson(person: PersonCreate): Person {
+        // POST request to `/persons`
+        // with person as the request body.
+        return null;
+    }
+
+    getPerson(personSlug: string): Person {
+        // GET request to `/person/${personSlug}`
+        return null;
+    }
+
+    updatePerson(personSlug: string, personPatch: PersonPatch): Person {
+        // PATCH Request to `person/${personSlug}`
+        // with personPatch as the request body
+        return null;
+    }
+
 }
